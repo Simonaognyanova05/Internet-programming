e1e722c24217a5d66cd25842398450b77a325716
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-11-14 08:50:21 +0200

    Intro to js

---
+++ Intro-to-JS/index.js
@@ -0,0 +1,4 @@
+let myYears = 18;
+let maxYears = 100;
+let year = ((maxYears - myYears) * 365) * 15;
+console.log(`Ще ви трябва ${year}, за да ви задържи до дълбока старост ${maxYears - myYears}`);
